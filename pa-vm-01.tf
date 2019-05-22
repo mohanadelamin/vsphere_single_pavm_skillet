2ae760796e63466c1a1458b2d554b9474d341c2e
Mohanad Elamin <mohanad.elamin@gmail.com>
2019-05-22 11:58:55 -0700

    variable edit

--- pa-vm-01.tf
+++ pa-vm-01.tf
@@ -3,9 +3,9 @@
 #===============================================================================
 provider "vsphere" {
   version        = "1.5.0"
-  vsphere_server = "${var.vsphere_vcenter}"
-  user           = "${var.vsphere_user}"
-  password       = "${var.vsphere_password}"
+  vsphere_server = "${var.vsphere_server}"
+  user           = "${var.user}"
+  password       = "${var.password}"
 
   allow_unverified_ssl = "true"
 }
