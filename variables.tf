3e6e1c223f58a4e265e690123195ae8fc9bc3885
Mohanad Elamin <mohanad.elamin@gmail.com>
2019-05-22 13:31:15 -0700

    Edit variables default values

--- variables.tf
+++ variables.tf
@@ -35,13 +35,11 @@ variable "vsphere_cluster" {
 }
 
 variable "vsphere_vcpu_number" {
-  description = "How many vCPU will be assigned to the VM (default: 2)"
-  default     = "2"
+  description = "How many vCPU will be assigned to the VM"
 }
 
 variable "vsphere_memory_size" {
-  description = "How much RAM will be assigned to the VM (default: 1024)"
-  default     = "4608"
+  description = "How much RAM will be assigned to the VM"
 }
 
 variable "vsphere_datastore" {
@@ -50,15 +48,12 @@ variable "vsphere_datastore" {
 
 variable "vsphere_port_group_mgmt" {
   description = "In which port group the VM NIC will be configured for management interface (default: VM Network)"
-  default     = "VM Network"
 }
 
 variable "vsphere_port_group_untrust" {
   description = "In which port group the VM NIC will be configured for untrust interface (default: VM Network)"
-  default     = "VM Network"
 }
 
 variable "vsphere_port_group_trust" {
   description = "In which port group the VM NIC will be configured for trust interface (default: VM Network)"
-  default     = "VM Network"
 }
